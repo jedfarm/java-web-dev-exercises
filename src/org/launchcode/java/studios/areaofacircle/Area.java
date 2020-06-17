02bd3e63184ef19afda76adf5616ac375d65b4c3
jedfarm <jfand72@gmail.com>
2020-06-16 20:55:13 -0400

    Studio plus bonus mission completed

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -2,9 +2,23 @@ package org.launchcode.java.studios.areaofacircle;
 import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
+        double radius;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
-        double radius = input.nextDouble();
-        System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius));
+        try{
+            radius = input.nextDouble();
+            while (radius < 0) {
+                System.out.println("Please, enter a positive number");
+                System.out.println("Enter a radius: ");
+                radius = input.nextDouble();
+            }
+            System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius));
+
+        } catch (Exception e) {
+            System.out.println("Please, enter a valid number");
+        }
+
+
+
     }
 }
