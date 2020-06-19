b0eb640f93c3cbf1adcf929daccaaac8b800a0d3
jedfarm <jfand72@gmail.com>
2020-06-18 20:53:54 -0400

    Studio finished with bonus mission

--- src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
+++ src/org/launchcode/java/studios/countingcharacters/CountingCharacters.java
@@ -2,13 +2,20 @@ package org.launchcode.java.studios.countingcharacters;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class CountingCharacters {
     public static void main(String[] args) {
-        String myString = "If the product of two terms is zero then common sense says at least one of the two terms has" +
-                " to be zero to start with. So if you move all the terms over to one side, you can put the quadratics " +
-                "into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done " +
-                "that, it’s pretty straightforward from there.";
+
+//        String myString = "If the product of two terms is zero then common sense says at least one of the two terms has" +
+//                " to be zero to start with. So if you move all the terms over to one side, you can put the quadratics " +
+//                "into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done " +
+//                "that, it’s pretty straightforward from there.";
+        // Bonus mission
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please, enter your text here:");
+        String myString = input.nextLine().toLowerCase();
+        input.close();
 
         Map<Character,Integer> charCounter = countChar(myString);
 
@@ -16,7 +23,10 @@ public class CountingCharacters {
             Character key = (Character)mapElement.getKey();
             int value = (int)mapElement.getValue();
 
-            System.out.println(key + " : " + value);
+            if (Character.isLetter(key)){
+                System.out.println(key + " : " + value);
+            }
+
         }
     }
     public static Map<Character, Integer> countChar(String text){
